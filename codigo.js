1448cace872d91b042f7356d9460934d9c039c9d
frank5000 <87660250+frank5000@users.noreply.github.com>
2022-02-11 16:03:34 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,18 @@
+function saludar() {
+
+	respuesta= prompt("Hola Franco, como estas");
+	if(respuesta =="bien")
+	{
+		alert("nashe");
+	}
+	else 
+	{
+		alert("la peor");
+	}
+}
+
+
+for( i=0; i<3; i++)
+{
+	saludar();
+}
